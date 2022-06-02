c778faae079322a98f4e538e9352c86ba53fafa0
Iacopo Spalletti <i.spalletti@nephila.it>
2022-06-02 12:35:16 +0200

    Update packages (#69)

--- populate.py
+++ populate.py
@@ -1,12 +1,13 @@
 import typer
 
+from book_search.app_settings import Settings
 from book_search.es import init_es
 from book_search.es.documents import Book
 from tests.utils import load_json_to_es
 
 
 def main(path: str):
-    es = init_es(use_async=False)
+    es = init_es(Settings(), use_async=False)
     load_json_to_es(es, path, Book)
 
 
