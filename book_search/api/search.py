fb544f36fef2b1be04d480717e6e7a96b395c558
Iacopo Spalletti <i.spalletti@nephila.it>
2023-05-26 18:34:41 +0200

    Update to FastAPI 0.95

--- book_search/api/search.py
+++ book_search/api/search.py
@@ -1,4 +1,6 @@
-from typing import List
+from __future__ import annotations
+
+from typing import Annotated
 
 from elasticsearch import AsyncElasticsearch
 from fastapi import APIRouter, Depends, Query
@@ -12,18 +14,19 @@ from ..es.search import search_es
 search_router = APIRouter()
 
 
-@search_router.get("/search/", response_model=models.BookListResponse, name="search")
+@search_router.get("/search/", name="search")
 async def search(
     request: Request,
-    settings: Settings = Depends(get_settings),  # noqa: B008
-    es_client: AsyncElasticsearch = Depends(get_es),  # noqa: B008
-    q: str = Query(None, description="Free text query string."),  # noqa: B008
-    tags: List[str] = Query([], description="List of tags slugs."),  # noqa: B008
-    year: int = Query(None, description="Filter by publication year."),  # noqa: B008
-    size: int = Query(20, description="Number of results in the current page."),  # noqa: B008
-):
+    settings: Settings = Depends(get_settings),
+    es_client: AsyncElasticsearch = Depends(get_es),
+    tags: Annotated[list[str] | None, Query(description="List of tags slugs.")] = None,
+    year: Annotated[int | None, Query(description="Filter by publication year.")] = None,
+    size: Annotated[int, Query(description="Number of results in the current page.")] = 20,
+    q: Annotated[str | None, Query(description="Free text query string.")] = None,
+) -> models.BookListResponse:
     """
     Search ES data according to the provided filters.
     """
+    tags = tags or []
     results, total = await search_es(settings, es_client, q, year, tags, size)
-    return {"results": results, "count": total}
+    return models.BookListResponse(results=results, count=total)
