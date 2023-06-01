17a0b2890762e33323db0dc0a4a0d08fc5afa05e
Iacopo Spalletti <i.spalletti@nephila.it>
2023-06-01 03:36:41 +0200

    Update to FastAPI 0.95 (#88)
    * Update to FastAPI 0.95
    
    * Add sample middleware
    
    * Fix tox.ini

--- book_search/main.py
+++ book_search/main.py
@@ -1,5 +1,8 @@
+import time
+
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from starlette.requests import Request
 
 from .api import api_router, root_router
 from .app_settings import get_settings  # noqa
@@ -19,3 +22,12 @@ if settings.BACKEND_CORS_ORIGINS:
 
 app.include_router(root_router)
 app.include_router(api_router, prefix=settings.API_V1_STR)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
---
+++ changes/87.feature
@@ -0,0 +1,1 @@
+Update to FastAPI 0.95
